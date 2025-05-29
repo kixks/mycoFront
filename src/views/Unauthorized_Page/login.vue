971bff715eaf6a23cc97880d539a31f4da478402
kixks <kenmentzy@gmail.com>
2025-05-29 14:46:47 +0800

    landing page ui/routes

--- src/views/Unauthorized_Page/login.vue
+++ src/views/Unauthorized_Page/login.vue
@@ -1,1 +1,14 @@
-<template>Ako ang login area</template>
+<template>
+  <v-text-field label="Label" class="input-design"></v-text-field>
+</template>
+
+<style scoped>
+.input-design {
+  background-color: #f0f0f0;
+  border-radius: 8px;
+  padding: 10px;
+  font-size: 16px;
+  color: #333;
+  border: 1px solid #ccc;
+}
+</style>
